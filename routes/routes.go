779af6f66a78b166a8093df1ac11c752462b5c2f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-15 18:22:39 -0300

    Criar Middleware para setar Content-type

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/brunosantanati/api-go-rest/controllers"
+	"github.com/brunosantanati/api-go-rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
